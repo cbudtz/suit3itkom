920dcc06d7aac207f2aa59966c19ac99bef9d430
chbu <chbu@dtu.dk>
2019-10-01 18:23:09 +0200

    Initial commit

---
+++ src/main/webapp/calculator.js
@@ -0,0 +1,19 @@
+function clear(form) {
+    form.A.value = "";
+    form.B.value = "";
+    form.C.value = "";
+}
+
+function validatedForm(form) {
+    debugger;
+    console
+    if (!form.A.value || !form.B.value) {
+        alert("Du skal udfylde et tal!");
+        return false;
+    }
+}
+
+function compute(form) {
+    if (!validatedForm(form)){return false;}
+    form.C.value = parseFloat(form.A.value) + parseFloat(form.B.value);
+}
---
+++ src/main/webapp/dynamisk.jsp
@@ -0,0 +1,17 @@
+<%@ page import="java.util.Date" %><%--
+  Created by IntelliJ IDEA.
+  User: chbu
+  Date: 27-09-2019
+  Time: 09:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+Hej Angelo. Klokken er <% out.println(new Date()); %>
+
+</body>
+</html>
